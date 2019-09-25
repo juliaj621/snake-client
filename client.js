844cee998a505bf97a74fc4dc9676b8a063e6848
Julia Jachimowicz <juliaj621@gmail.com>
2019-09-25 16:01:20 -0700

    final changes to snake game

--- client.js
+++ client.js
@@ -1,15 +1,14 @@
 const net = require('net');
-
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function () {
   const conn = net.createConnection({
-    // host for computer version of game
-    // host: '10.0.2.15',
+    // host for computer version of game -- > host: '10.0.2.15',
     // host for classroom version of game
-    host: '192.168.88.254',
-    port: 50541,
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
@@ -19,22 +18,10 @@ const connect = function () {
   });
 
   conn.on('connect', () => {
-    console.log('Successfully connected to game server')
-    conn.write("Name: JJ")
-  })
-
-  // conn.on('connect', () => {
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 1000);
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 2000);
-  // })
-
-
-
+    console.log('Successfully connected to game server');
+    conn.write("Name: JJ");
+  });
   return conn;
-}
+};
 
-module.exports = { connect }
+module.exports = { connect };
---
+++ constants.js
@@ -0,0 +1,7 @@
+const PORT = 50541
+const IP = '192.168.88.254'
+
+module.exports = {
+  IP,
+  PORT
+};
