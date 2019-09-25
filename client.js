ac6465bd72655235af613fc0f773e64691c3bf33
Julia Jachimowicz <juliaj621@gmail.com>
2019-09-25 13:34:27 -0700

    adding connection messages to game

--- client.js
+++ client.js
@@ -5,15 +5,23 @@ const net = require('net');
  */
 const connect = function () {
   const conn = net.createConnection({
-    host: '10.0.2.15',
+    // host for computer version of game
+    // host: '10.0.2.15',
+    // host for classroom version of game
+    host: '192.168.88.254',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
-  conn.on('data', (data) => {
-    console.log("you ded cuz you idled", data);
+  conn.on('data', () => {
+    console.log("you ded cuz you idled");
   });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server')
+    conn.write("Name: JJ")
+  })
 
   return conn;
 }
