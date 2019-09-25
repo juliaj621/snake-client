fbee53a9490cb8cd77f5ea2d0c5d69952190afae
Julia Jachimowicz <juliaj621@gmail.com>
2019-09-25 15:42:57 -0700

    implementing direction keys for snake

--- client.js
+++ client.js
@@ -37,4 +37,4 @@ const connect = function () {
   return conn;
 }
 
-module.exports = connect
+module.exports = { connect }
--- input.js
+++ input.js
@@ -1,8 +1,11 @@
+// Stores the active TCP connection object.
+let connection;
 /**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function () {
+* Setup User Interface
+* Specifically, so that we can handle user input via stdin
+*/
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -15,6 +18,18 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
 };
 
-module.exports = setupInput
+module.exports = { setupInput }
