bcb12445fd97f8df0e3119d15bebcb47530444d7
Julia Jachimowicz <juliaj621@gmail.com>
2019-09-25 15:00:39 -0700

    creating a new input file for key commands

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 const connect = require('./client');
+const setupInput = require('./input');
 console.log('Connecting ...');
 connect();
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
